3f8f6f28d94f327261ef1749d187935897e72622
sergey.murashow <sergey.murashow@gmail.com>
2025-04-21 16:32:08 +0400

    Kaef

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,31 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+local telescope = require("telescope.builtin")
+local plenary_scan = require("plenary.scandir")
+
+vim.api.nvim_create_user_command("SwapFiles", function()
+  local swap_dir = vim.fn.stdpath("state") .. "/swap/" -- Директория для swap-файлов
+  if vim.fn.isdirectory(swap_dir) == 0 then
+    print("Swap directory does not exist: " .. swap_dir)
+    return
+  end
+
+  local files = plenary_scan.scan_dir(swap_dir, { depth = 1, add_dirs = false, hidden = true })
+
+  telescope.find_files({
+    prompt_title = "Swap Files",
+    cwd = swap_dir,
+    attach_mappings = function(_, map)
+      map("i", "<C-dl>", function(prompt_bufnr)
+        local entry = require("telescope.actions.state").get_selected_entry()
+        local file = entry.path
+        local result = vim.fn.delete(file) -- Удаляем файл
+        if result == 0 then
+          print("Deleted: " .. file)
+        else
+          print("Failed to delete: " .. file)
+        end
+        require("telescope.actions").close(prompt_bufnr)
+      end)
+      return true
+    end,
+  })
+end, { desc = "Find and manage swap files" })
--- nvim/lua/plugins/dap-plugin.lua
+++ nvim/lua/plugins/dap-plugin.lua
@@ -15,6 +15,9 @@ return {
       vim.keymap.set("n", "<leader>dc", function()
         require("dap").continue()
       end, { desc = "Debug: Start/Continue" })
+
+      vim.keymap.set("n", "<leader>dd", ":DapDisconnect<CR>==", { desc = "Debug: Disconnect session" })
+
       vim.keymap.set("n", "<leader>dt", function()
         require("dap").toggle_breakpoint()
       end, { desc = "Debug: Toggle Breakpoint" })
@@ -30,6 +33,67 @@ return {
       vim.keymap.set("n", "<leader>dr", function()
         require("dap").repl.open()
       end, { desc = "Debug: Open REPL" })
+      vim.keymap.set("n", "<leader>dh", function()
+        local widgets = require("dap.ui.widgets")
+        widgets.preview()
+      end, { desc = "Debug: Preview vars" })
+      vim.keymap.set("n", "<Leader>ds", function()
+        local widgets = require("dap.ui.widgets")
+        widgets.centered_float(widgets.scopes)
+      end, { desc = "Debug: Scopes" })
+
+      -- Trying some features
+
+      local dap = require("dap")
+      local widgets = require("dap.ui.widgets")
+
+      -- Переопределение поведения REPL
+      dap.defaults.fallback.terminal_win_cmd = "botright 10split new" -- Отключаем стандартное окно
+
+      -- Настройка запуска дебага без отображения окон
+      dap.listeners.before.event_initialized["custom_behavior"] = function()
+        -- Не открываем окна автоматически
+      end
+
+      -- Закрытие окна дебага после завершения сессии
+      dap.listeners.after.event_terminated["close_debug"] = function()
+        dap.close() -- Закрывает все окна, связанные с дебагом
+      end
+
+      dap.listeners.after.event_exited["close_debug"] = function()
+        dap.close() -- Закрывает окна, если сессия завершилась
+      end
+
+      -- Открыть панель с переменными посередине
+      vim.keymap.set("n", "<Leader>dv", function()
+        widgets.centered_float(widgets.scopes).toggle()
+      end)
+
+      -- Открыть вывод дебага снизу
+      vim.keymap.set("n", "<Leader>do", function()
+        vim.cmd("botright split | resize 10") -- Создаем окно снизу
+        dap.repl.open(nil, "split") -- Открываем вывод в этом окне
+      end)
+
+      -- Открыть REPL как всплывающее окно
+      vim.keymap.set("n", "<Leader>dr", function()
+        widgets.centered_float(require("dap.repl").open()) -- Открытие REPL во всплывающем окне
+      end)
+
+      -- Закрытие окна по нажатию 'q'
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "dap-float",
+        callback = function()
+          vim.api.nvim_buf_set_keymap(0, "n", "q", "<Cmd>close<CR>", { noremap = true, silent = true })
+        end,
+      })
+
+      -- Автозапуск панелей при старте дебага
+      -- dap.listeners.after.event_initialized['custom_ui'] = function()
+      --   widgets.sidebar(widgets.scopes).open()
+      --   vim.cmd('botright split | resize 10')
+      --   dap.repl.open(nil, 'split')
+      -- end
     end,
     config = function()
       require("config.dap-config").setup()
